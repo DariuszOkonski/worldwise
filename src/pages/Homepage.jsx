c626864f62b64a41661987c706fdde851277a1eb
Dariusz Okoński <darek200180@gmail.com>
2024-03-21 19:35:17 +0100

    add PageNav

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-
 function Homepage() {
-  return <div>WorldWise</div>;
+  return (
+    <div>
+      <h1>WorldWise</h1>
+    </div>
+  );
 }
 
 export default Homepage;
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Product() {
-  return <div>Product</div>;
+  return (
+    <div>
+      <h1>Product</h1>
+    </div>
+  );
 }
 
 export default Product;
